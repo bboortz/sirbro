9367a2dc5dc0d463c08598c9c69b491985a3b682
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2016-09-09 07:16:49 +0200

    application config improved /BB

--- sirbro_lib/sirbro_lib/appconfig.py
+++ sirbro_lib/sirbro_lib/appconfig.py
@@ -2,11 +2,27 @@ import os, platform
 
 from sirbro_lib.flaskconfig import DefaultFlaskConfig
 
-class DefaultAppConfig(object):
+
+
+class DefaultBaseConfig(object):
     ENV = os.getenv('ENV', 'PROD')
+    PYTHONVERSION = platform.python_version()
+    
+    def config_to_json(self):
+        result = { 
+            'ENV': self.ENV,
+            'python-version': self.PYTHONVERSION,
+        }
+        return result
+
+
+class DefaultCliConfig(DefaultBaseConfig):
+    BASE_URL = os.getenv('BASE_URL', 'http://localhost:8080/api')
+
+
+class DefaultAppConfig(DefaultBaseConfig):
     IP = os.getenv('IP', '0.0.0.0')
     PORT = int( os.getenv('PORT', 8080) )
-    PYTHONVERSION = platform.python_version()
     FLASKCONFIG = DefaultFlaskConfig
     
     def config_to_json(self):
@@ -20,3 +36,4 @@ class DefaultAppConfig(object):
             'flask-testing': self.FLASKCONFIG.TESTING ,
         }
         return result
+
