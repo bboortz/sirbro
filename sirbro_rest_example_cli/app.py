ad754ca272b0687840bb90fc2bb5fc5f626e5f33
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2016-09-11 20:08:24 +0200

    command line parsing improved /BB

--- sirbro_rest_example_cli/app.py
+++ sirbro_rest_example_cli/app.py
@@ -3,8 +3,9 @@
 import os
 import sys
 
-from sirbro_app import __projname__, __projver__, __projdesc__
-from sirbro_app.app import run_app
+from sirbro_app import __projname__, __projver__, __projdesc__, __apiversions__
+from sirbro_app.app import *
+from sirbro_lib import __projname__ as __libname__, __projver__ as __libver__
 from sirbro_lib.logger import getLogger
 
 import argparse
@@ -25,61 +26,33 @@ LOGGER = getLogger('argparser')
 
 # define the parser arguments
 parser = argparse.ArgumentParser(prog=__projname__, description=__projdesc__)
-parser.add_argument('-V', '--version', action='version', version='%(prog)s {version}'.format(version=__projver__))
+parser.add_argument('-V', '--version', action='version', version='%(prog)s {version} - apiversions: {apiversions}'.format(version=__projver__, apiversions=__apiversions__))
 subparsers = parser.add_subparsers(dest="subparser_name") 
 api_parser = subparsers.add_parser('api', help='api info')
 api_group = api_parser.add_mutually_exclusive_group(required=True)
-api_group.add_argument('--alive', action='store_true', help='get api alive status')
-api_group.add_argument('--info', action='store_false', help='get api info')
-api_group.add_argument('--config', action='store_false', help='get api config')
-#api_parser.add_argument('--ALIVE', help='get api alive status')
-#api_parser.add_argument('info', help='get api info')
-#api_parser.add_argument('config', help='get api config')
-#api_info_parser = api_parser.add_subparsers(dest="info") 
-#api_info_parser.add_argument('--alive', help='get api alive status')
-#group = api_parser.add_argument_group('group')
-#group.add_argument('--foo', help='foo help')
-#group.add_argument('bar', help='bar help')
+api_group.add_argument('-a', '--alive', action='store_true', help='get api alive status')
+api_group.add_argument('-i', '--info', action='store_true', help='get api info')
+api_group.add_argument('-c', '--config', action='store_true', help='get api config')
 resource_parser = subparsers.add_parser('resource', help='manage a resource')
-resource_parser.add_argument('-c', '--create', dest='create', help='create the resource')
-resource_parser.add_argument('-d', '--delete', dest='delete', help='delete the resource')
+resource_group = resource_parser.add_mutually_exclusive_group(required=True)
+resource_group.add_argument('-c', '--create', action='store_true', help='create the resource')
+resource_group.add_argument('-d', '--delete', action='store_true', help='delete the resource')
 resource_parser.add_argument('-f', '--file', dest='config_file', help='specify the config file')
-#run_parser.add_argument('CONTAINER', help='the container to run')
 
 
 
-# parse the arguments
-if len(sys.argv)==1:
-    parser.print_help()
-    sys.exit(1)
-
-args = vars( parser.parse_args() )
-LOGGER.debug(args)
-
-if 'subparser_name' not in args:
-    parser.print_help()
-    sys.exit(1)
-subparser_name = args['subparser_name']
-
-
+#
+# * functions*
+#
 def retrieveArgument(args, key, default=None):
     if key not in args  or  args[key] == None:
         return default
     return args[key]
 
 
-# parse the subparser / commands
-try:
-    if subparser_name == 'api':
-            LOGGER.info("api")
-    elif subparser_name == 'resource':
-            LOGGER.info("resource")
-    else:
-        parser.print_help()
-        sys.exit(1)
-
-except KeyboardInterrupt:
-    LOGGER.info("^C")
+def exit_with_help():
+    parser.print_help()
+    sys.exit(1)
 
 
 
@@ -87,7 +60,46 @@ except KeyboardInterrupt:
 # * main routine *
 #
 def main():
-    run_app()
+    LOGGER.info("%s %s starting..." % (__projname__, __projver__) )
+    LOGGER.debug("using %s %s." % (__libname__, __libver__) )
+    LOGGER.info("BASE_URL is %s." % (AppConfig.BASE_URL) )
+
+    # parse the arguments
+    if len(sys.argv)==1:
+        exit_with_help()
+
+    args = vars( parser.parse_args() )
+    LOGGER.debug(args)
+
+    if 'subparser_name' not in args:
+        exit_with_help
+    subparser_name = args['subparser_name']
+
+
+
+
+    # parse the subparser / commands
+    try:
+        if subparser_name == 'api':
+            if args['alive']:
+                api_get_alive()
+            elif args['info']:
+                api_get_info()
+            elif args['config']:
+                api_get_config()
+            else:
+                exit_with_help()
+            
+        elif subparser_name == 'resource':
+                LOGGER.info("resource")
+        else:
+            parser.print_help()
+            sys.exit(1)
+
+    except KeyboardInterrupt:
+        LOGGER.info("^C")
+
+    LOGGER.info("%s %s stopped." % (__projname__, __projver__) )
 
 if __name__ == "__main__":
     main()
--- sirbro_rest_example_cli/sirbro_app/app.py
+++ sirbro_rest_example_cli/sirbro_app/app.py
@@ -1,7 +1,7 @@
 
 from sirbro_app import __projname__, __projver__
 from sirbro_app.appconfig import AppConfig
-from sirbro_lib import __projname__ as __libname__, __projver__ as __libver__
+from sirbro_app.api.apiv1 import Client
 from sirbro_lib.logger import getLogger
 
 
@@ -11,20 +11,15 @@ from sirbro_lib.logger import getLogger
 #
 LOGGER = getLogger('app')
 appconfig = AppConfig
+client = Client()
 
 
 
 #
 # * run the app
 #
-def run_app():
-    LOGGER.info("%s %s starting..." % (__projname__, __projver__) )
-    LOGGER.debug("using %s %s." % (__libname__, __libver__) )
-    LOGGER.info("BASE_URL is %s." % (AppConfig.BASE_URL) )
+def api_get_info():
+    result = client.get_info()
+    print( result )
 
-
-    LOGGER.info("%s %s stopped." % (__projname__, __projver__) )
-
-def get_app():
-    return app
 
